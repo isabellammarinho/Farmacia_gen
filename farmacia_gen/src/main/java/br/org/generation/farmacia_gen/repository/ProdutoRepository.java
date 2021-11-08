50552723d5f5c1845aba247684d138f937095b0e
Isabella Marinho <isabellamarcelomarinho@gmail.com>
2021-11-08 19:07:29 -0300

    Projeto Farmácia_Gen

---
+++ farmacia_gen/src/main/java/br/org/generation/farmacia_gen/repository/ProdutoRepository.java
@@ -0,0 +1,33 @@
+package br.org.generation.farmacia_gen.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.farmacia_gen.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List <Produto> findAllByNomeContainingIgnoreCase(String nome);
+	
+	
+	  // MySQL: select * from tb_produtos where nome = "produto" and laboratorio = "laboratorio";
+	 
+	 
+	public List <Produto> findByNomeAndLaboratorio(String nome, String laboratorio);
+	
+	
+	 // MySQL: select * from tb_produtos where nome = "produto" or laboratorio = "laboratorio";
+	 
+	public List <Produto> findByNomeOrLaboratorio(String nome, String laboratorio);
+
+	@Query(value = "select * from tb_produtos where preco between :inicio and :fim", nativeQuery = true)
+	public List <Produto> buscarProdutosEntre(@Param("inicio") BigDecimal inicio, @Param("fim") BigDecimal fim);
+}
+
+
---
+++ farmacia_gen/src/test/java/br/org/generation/farmacia_gen/FarmaciaGenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.farmacia_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
